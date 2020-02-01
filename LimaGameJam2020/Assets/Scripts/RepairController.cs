e7160d48522b68ca91137d178de65ad1fff7da7d
--replace-all <seb_110@hotmail.com>
2020-02-01 00:01:08 -0500

    Probando movimiento

--- LimaGameJam2020/Assets/Scripts/RepairController.cs
+++ LimaGameJam2020/Assets/Scripts/RepairController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class RepairController : MonoBehaviour
 {
+
+    public ControladorMando controladorMando;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(new Vector3(controladorMando.LeftJoystick().x, controladorMando.LeftJoystick().y, 0) * Time.deltaTime);
     }
 }
