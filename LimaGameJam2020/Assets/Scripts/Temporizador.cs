1fa9d693a6e9c775811c91ebbb8616439c23bf59
Escombraura <seb_110@hotmail.com>
2020-02-01 14:05:42 -0500

    Temporizador

---
+++ LimaGameJam2020/Assets/Scripts/Temporizador.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Temporizador
+{
+
+    public static float time = 0f;
+    public static bool active = false;
+    public static bool ready = false;
+
+
+
+    public static void Active(float _time)
+    {
+        if (ready == false) return;
+
+        active = true;
+        time = _time;
+    }
+
+    public static void Finish()
+    {
+        ready = false;
+    }
+
+
+    static IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(time);
+        ready = true;
+    }
+
+}
