210790a5dc4362e9ff60c6c6c7468ca0b33eaee1
Luis Pineda <luis.alonso97@outlook.com>
2020-02-01 12:29:13 -0500

    Khe

--- LimaGameJam2020/Assets/Scripts/Player1.cs
+++ LimaGameJam2020/Assets/Scripts/Player1.cs
@@ -64,10 +64,13 @@ public class Player1 : MonoBehaviour {
 
     void OnTriggerStay2D (Collider2D other) {
         //Verifica que el objeto es agarrable xD
-        if (other.tag == "Objeto" && ControladorMando.PressA ()  && hijo == null) {
+        if (other.tag == "Objeto") {
             Debug.Log ("cogible");
-            hijo = other.transform;
-            hijo.parent = transform;
+            if (ControladorMando.PressA () && hijo == null) {
+                // Debug.Log ("cogible");
+                hijo = other.transform;
+                hijo.parent = transform;
+            }
         }
     }
 
