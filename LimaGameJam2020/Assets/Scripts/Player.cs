6a5701f459724ede0c7447976f6e6ce26f01b943
Escombraura <seb_110@hotmail.com>
2020-02-02 13:31:58 -0500

    Correccion de canvas

--- LimaGameJam2020/Assets/Scripts/Player.cs
+++ LimaGameJam2020/Assets/Scripts/Player.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     public Transform robot;
     public GameObject[] eventos;
@@ -20,118 +21,142 @@ public class Player : MonoBehaviour {
     private GameManager gmManager;
     public Text texto;
     // Start is called before the first frame update
-    void Start () {
+    void Start()
+    {
         // if (eventoGiro) accesoGiro = eventoGiro.GetComponent<EventoGiro> ();
         // if (eventoPresion) accesoPresion = eventoPresion.GetComponent<EventoPresionar> ();
 
-        if (!gmManager) gmManager = GameObject.Find ("Manager").GetComponent<GameManager> ();
-        SetEvent ();
+        if (!gmManager) gmManager = GameObject.Find("Manager").GetComponent<GameManager>();
+        SetEvent();
 
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         //Pos te mueves
-        if (background != true) transform.Translate (new Vector3 (ControladorMando.LeftJoystick (ID).x, ControladorMando.LeftJoystick (ID).y, 0) * Time.deltaTime * velocidad);
+        if (background != true) transform.Translate(new Vector3(ControladorMando.LeftJoystick(ID).x, ControladorMando.LeftJoystick(ID).y, 0) * Time.deltaTime * velocidad);
 
         //Soltar objeto
-        if (ControladorMando.PressRT (ID) < 1 && hijo != null) {
+        if (ControladorMando.PressRT(ID) < 1 && hijo != null)
+        {
             //Verficia si estas encima del robot y si ya hay un objeto soldado
-            if (enRobot && robot.Find ("Pieza" + ID).childCount == 0) {
-                SoundController.PlayOtherSoundEfect (Random.Range (4, 7));
+            if (enRobot && robot.Find("Pieza" + ID).childCount == 0)
+            {
+                SoundController.PlayOtherSoundEfect(Random.Range(4, 7));
 
-                hijo.parent = robot.Find ("Pieza" + ID);
-                hijo.position = robot.Find ("Pieza" + ID).transform.position;
+                hijo.parent = robot.Find("Pieza" + ID);
+                hijo.position = robot.Find("Pieza" + ID).transform.position;
                 hijo.gameObject.name = ("Ext" + ID);
                 hijo = null;
-                objetoEvento = Instantiate (eventos[eventID]).transform;
-                accesoEvento = objetoEvento.GetComponent<Evento> ();
-                ComenzarEvento (eventID);
-            } else {
+                objetoEvento = Instantiate(eventos[eventID]).transform;
+                accesoEvento = objetoEvento.GetComponent<Evento>();
+                ComenzarEvento(eventID);
+            }
+            else
+            {
                 hijo.parent = null;
                 hijo = null;
             }
         }
     }
 
-    public void SetEvent () {
-        eventID = Random.Range (0, eventos.Length);
+    public void SetEvent()
+    {
+        eventID = Random.Range(0, eventos.Length);
     }
 
-    public void ComenzarEvento (int eventoID) {
+    public void ComenzarEvento(int eventoID)
+    {
         //SoundController.PlaySoundEfectLoop(Random.Range(4, 7));
-        SoundController.PlayOtherSoundEfect (Random.Range (4, 7));
+        SoundController.PlayOtherSoundEfect(Random.Range(4, 7));
         background = true;
-        objetoEvento.gameObject.SetActive (true);
-        GetComponent<SpriteRenderer> ().enabled = false;
-        Debug.Log (feedBack.Length);
-        Debug.Log (eventID);
+        objetoEvento.gameObject.SetActive(true);
+        GetComponent<SpriteRenderer>().enabled = false;
+        Debug.Log(feedBack.Length);
+        Debug.Log(eventID);
 
-        feedBack[eventID].transform.Find (ID + "FeedBack").gameObject.SetActive (true);
-        StartCoroutine (FinalizarEvento (eventoID));
+        feedBack[eventID].transform.Find(ID + "FeedBack").gameObject.SetActive(true);
+        StartCoroutine(FinalizarEvento(eventoID));
     }
 
-    void OnTriggerStay2D (Collider2D other) {
+    void OnTriggerStay2D(Collider2D other)
+    {
         //Verifica que el objeto es agarrable xD
-        if (other.tag == "Objeto") {
-            if (ControladorMando.PressRT (ID) == 1 && hijo == null) {
+        if (other.tag == "Objeto")
+        {
+            if (ControladorMando.PressRT(ID) == 1 && hijo == null)
+            {
                 if (other.transform.parent && other.transform.parent.tag == "Jugador" + ID) return;
-                SoundController.PlayOtherSoundEfect (9);
+                SoundController.PlayOtherSoundEfect(9);
                 hijo = other.transform;
                 hijo.parent = transform;
             }
         }
     }
 
-    void OnTriggerEnter2D (Collider2D other) {
-        if (other.tag == ("Jugador" + ID)) {
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == ("Jugador" + ID))
+        {
             enRobot = true;
         }
     }
 
-    void OnTriggerExit2D (Collider2D other) {
+    void OnTriggerExit2D(Collider2D other)
+    {
         if (other.tag == "Jugador" + ID)
             enRobot = false;
     }
 
-    IEnumerator FinalizarEvento (int eventoID) {
-        switch (eventoID) {
+    IEnumerator FinalizarEvento(int eventoID)
+    {
+        switch (eventoID)
+        {
             case 0:
-                do {
-                    yield return new WaitForSeconds (0.1f);
-                } while (accesoEvento.gameObject.GetComponent<EventoGiro> ().vuelta != accesoEvento.objetivo);
-                if (accesoEvento.falla) {
-                    SoundController.PlayOtherSoundEfect (7);
-                    objetoEvento.gameObject.SetActive (false);
-                    robot.Find ("Pieza" + ID).transform.Find ("Ext" + ID).gameObject.SetActive (false);
+                do
+                {
+                    yield return new WaitForSeconds(0.1f);
+                } while (accesoEvento.gameObject.GetComponent<EventoGiro>().vuelta != accesoEvento.objetivo);
+                if (accesoEvento.falla)
+                {
+                    SoundController.PlayOtherSoundEfect(7);
+                    objetoEvento.gameObject.SetActive(false);
+                    robot.Find("Pieza" + ID).transform.Find("Ext" + ID).gameObject.SetActive(false);
                     //robot.Find ("Pieza" + ID).transform.Find ("Ext" + ID).transform.parent = null;
-                    Destroy (robot.Find ("Pieza" + ID).transform.Find ("Ext" + ID).gameObject);
-                } else {
-                    AddScore ();
-                    SoundController.PlayOtherSoundEfect (13);
+                    Destroy(robot.Find("Pieza" + ID).transform.Find("Ext" + ID).gameObject);
+                }
+                else
+                {
+                    AddScore();
+                    SoundController.PlayOtherSoundEfect(13);
 
                 }
-                feedBack[eventID].transform.Find (ID + "FeedBack").gameObject.SetActive (false);
-                objetoEvento.gameObject.SetActive (false);
-                GetComponent<SpriteRenderer> ().enabled = true;
+                feedBack[eventID].transform.Find(ID + "FeedBack").gameObject.SetActive(false);
+                objetoEvento.gameObject.SetActive(false);
+                GetComponent<SpriteRenderer>().enabled = true;
                 background = false;
                 break;
             case 1:
-                do {
-                    yield return new WaitForSeconds (0.1f);
-                } while (accesoEvento.gameObject.GetComponent<EventoPresionar> ().resta != 0);
-                if (accesoEvento.falla) {
-                    accesoEvento.gameObject.SetActive (false);
-                    SoundController.PlayOtherSoundEfect (7);
-                    robot.Find ("Pieza" + ID).transform.Find ("Ext" + ID).gameObject.SetActive (false);
-                    robot.Find ("Pieza" + ID).transform.Find ("Ext" + ID).transform.parent = null;
-                } else {
-                    AddScore ();
-                    SoundController.PlayOtherSoundEfect (13);
+                do
+                {
+                    yield return new WaitForSeconds(0.1f);
+                } while (accesoEvento.gameObject.GetComponent<EventoPresionar>().resta != 0);
+                if (accesoEvento.falla)
+                {
+                    accesoEvento.gameObject.SetActive(false);
+                    SoundController.PlayOtherSoundEfect(7);
+                    robot.Find("Pieza" + ID).transform.Find("Ext" + ID).gameObject.SetActive(false);
+                    robot.Find("Pieza" + ID).transform.Find("Ext" + ID).transform.parent = null;
                 }
-                feedBack[eventID].transform.Find (ID + "FeedBack").gameObject.SetActive (false);
-                accesoEvento.gameObject.SetActive (false);
-                GetComponent<SpriteRenderer> ().enabled = true;
+                else
+                {
+                    AddScore();
+                    SoundController.PlayOtherSoundEfect(13);
+                }
+                feedBack[eventID].transform.Find(ID + "FeedBack").gameObject.SetActive(false);
+                accesoEvento.gameObject.SetActive(false);
+                GetComponent<SpriteRenderer>().enabled = true;
                 background = false;
                 break;
         }
@@ -139,24 +164,26 @@ public class Player : MonoBehaviour {
 
         //AddScore ();
 
-        feedBack[eventID].transform.Find (ID + "FeedBack").gameObject.SetActive (false);
-        SetEvent ();
+        feedBack[eventID].transform.Find(ID + "FeedBack").gameObject.SetActive(false);
+        SetEvent();
     }
 
-    private void AddScore () {
+    private void AddScore()
+    {
         score += 5;
-        Pintador _piezaSeleccionada = robot.Find ("Pieza" + ID).GetComponentInChildren<Pintador> ();
-        ColoScore (_piezaSeleccionada);
-        RobotScore (_piezaSeleccionada);
-        GameManager.gm.AddGlobalScore (score);
+        Pintador _piezaSeleccionada = robot.Find("Pieza" + ID).GetComponentInChildren<Pintador>();
+        ColoScore(_piezaSeleccionada);
+        RobotScore(_piezaSeleccionada);
+        GameManager.gm.AddGlobalScore(score);
     }
 
-    private void ColoScore (Pintador _piezaSeleccionada) {
-        int _robotIndice = robot.GetComponent<Pintador> ().indice;
+    private void ColoScore(Pintador _piezaSeleccionada)
+    {
+        int _robotIndice = robot.GetComponent<Pintador>().indice;
         int _piezaIndice = 0;
 
         _piezaIndice = _piezaSeleccionada.indice;
-        GameManager.gm.AddPlayerData (ID, _piezaIndice);
+        GameManager.gm.AddPlayerData(ID, _piezaIndice);
         /*
 
         switch (eventID)
@@ -169,44 +196,68 @@ public class Player : MonoBehaviour {
         }
         */
 
-        int _resultado = Mathf.Abs (_robotIndice - _piezaIndice);
+        int _resultado = Mathf.Abs(_robotIndice - _piezaIndice);
 
-        switch (_resultado) {
+        switch (_resultado)
+        {
 
             case 0:
                 score += 5;
+                Debug.Log("Es del mismo color");
                 break;
             case 1:
                 score += 1;
+                Debug.Log("Es de color adyacente");
                 break;
             case 3:
                 score += 3;
+                Debug.Log("Es de color adyacente");
                 break;
         }
     }
-    private void RobotScore (Pintador _piezaSeleccionada) {
-        Nombre _robot = robot.GetComponent<Pintador> ().nombre;
+    private void RobotScore(Pintador _piezaSeleccionada)
+    {
+        Nombre _robot = robot.GetComponent<Pintador>().nombre;
 
-        GameManager.gm.AddPlayerData (ID, _robot);
-        GameManager.gm.AddPlayerData (ID, _piezaSeleccionada.jugadorID);
+        GameManager.gm.AddPlayerData(ID, _robot);
+        GameManager.gm.AddPlayerData(ID, _piezaSeleccionada.jugadorID);
 
         if (_piezaSeleccionada.nombre == _robot)
+        {
             score += 5;
-
-        Debug.Log (_piezaSeleccionada.jugadorID == "Brazo");
-        Debug.Log (ID);
-
-        if (_piezaSeleccionada.piezaSimetrica) {
-            if (_piezaSeleccionada.jugadorID == "Brazo") {
-                if (ID == "" || ID == "2P")
-                    score += 5;
-            } else if (_piezaSeleccionada.jugadorID == "Pierna") {
-                if (ID == "3P" || ID == "4P")
-                    score += 5;
-            }
-        } else if (_piezaSeleccionada.jugadorID == ID) {
-            score += 5;
+            Debug.Log("La pieza es del mismo robot");
         }
+
+        Debug.Log(_piezaSeleccionada.jugadorID);
+        Debug.Log(ID);
+
+        if (_piezaSeleccionada.piezaSimetrica)
+        {
+            if (_piezaSeleccionada.jugadorID == "Brazo" || _piezaSeleccionada.jugadorID == "Brazos")
+            {
+                if (ID == "" || ID == "2P")
+                {
+                    score += 5;
+                    Debug.Log("Es la pieza correcta");
+                }
+
+            }
+            else if (_piezaSeleccionada.jugadorID == "Pierna" || _piezaSeleccionada.jugadorID == "Piernas")
+            {
+                if (ID == "3P" || ID == "4P")
+                {
+                    score += 5;
+                    Debug.Log("Es la pieza correcta");
+                }
+            }
+        }
+        else if (_piezaSeleccionada.jugadorID == ID)
+        {
+            score += 5;
+            Debug.Log("Es la pieza correcta");
+        }
+
+        texto.text = score.ToString();
 
     }
 }
