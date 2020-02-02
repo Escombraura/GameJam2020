d51c2a4b75887d477e7606e1fae98d3b5bd53fda
Escombraura <seb_110@hotmail.com>
2020-02-01 23:17:38 -0500

    Aplicando el sonido

--- LimaGameJam2020/Assets/Scripts/EventoGiro.cs
+++ LimaGameJam2020/Assets/Scripts/EventoGiro.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EventoGiro : Evento {
+public class EventoGiro : Evento
+{
     public int verificador;
 
     public int vuelta;
@@ -14,30 +15,38 @@ public class EventoGiro : Evento {
     //public Slider slider;
 
     // Update is called once per frame
-    void Update () {
-        if (tiempoLimite - Time.deltaTime > 0) {
+    void Update()
+    {
+        if (tiempoLimite - Time.deltaTime > 0)
+        {
             tiempoLimite -= Time.deltaTime;
-            if (ControladorMando.RightJoystick (ID) == Vector2.zero) return;
+            if (ControladorMando.RightJoystick(ID) == Vector2.zero) return;
 
             anguloActual = transform.eulerAngles.z;
-            transform.rotation = Quaternion.Euler (0, 0, Mathf.Atan2 (-ControladorMando.RightJoystick (ID).x, ControladorMando.RightJoystick (ID).y) * 180 / Mathf.PI);
+            transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(-ControladorMando.RightJoystick(ID).x, ControladorMando.RightJoystick(ID).y) * 180 / Mathf.PI);
             angulocomparar = transform.eulerAngles.z;
 
-            if (anguloActual > angulocomparar) {
+            if (anguloActual > angulocomparar)
+            {
                 verificador++;
+                SoundController.PlayOtherSoundEfect(1);
+
                 if (verificador < -5) vuelta--;
                 if (verificador < 0) verificador = 0;
-            } else if (anguloActual < angulocomparar) //Giro opuesto
+            }
+            else if (anguloActual < angulocomparar) //Giro opuesto
             {
                 verificador--;
                 if (verificador > 5) vuelta++;
                 if (verificador > 0) verificador = 0;
             }
             //slider.value = vuelta;
-        } else { falla = true; vuelta = (int) objetivo; return; }
+        }
+        else { falla = true; vuelta = (int)objetivo; return; }
     }
 
-    public void ReiniciarVeulta () {
+    public void ReiniciarVeulta()
+    {
         vuelta = 0;
     }
 }
