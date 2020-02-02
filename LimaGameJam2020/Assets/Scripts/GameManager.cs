4c800832946423759b75c1fdb16189cfa862b5c1
Escombraura <seb_110@hotmail.com>
2020-02-02 14:18:43 -0500

    Puntaje Global Actualizado

--- LimaGameJam2020/Assets/Scripts/GameManager.cs
+++ LimaGameJam2020/Assets/Scripts/GameManager.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
     public GameObject[] Robots;
     public GameObject RobotActual;
     public Player[] Jugadores;
@@ -23,91 +25,125 @@ public class GameManager : MonoBehaviour {
     public int[] verificadorColor;
     private float multiplicador;
 
+    public Text texto;
+    public GameObject panelGlobal;
+
     // Start is called before the first frame update
-    void Start () {
+    void Start()
+    {
         if (!gm) gm = this;
         estado = GameState.setup;
 
-        Debug.Log ("llamar");
-        temporizador.Activar (5f);
-        for (int i = 0; i <= 11; i++) {
-            if (faja[i].childCount == 1) {
-                Instantiate (partes[Random.Range (0, partes.Length)], faja[i].position, faja[i].rotation, faja[i]);
+        Debug.Log("llamar");
+        temporizador.Activar(5f);
+        for (int i = 0; i <= 11; i++)
+        {
+            if (faja[i].childCount == 1)
+            {
+                Instantiate(partes[Random.Range(0, partes.Length)], faja[i].position, faja[i].rotation, faja[i]);
             }
         }
     }
     // Update is called once per frame
-    void Update () {
-        if (temporizador.GetStage () == 0) {
-            if (temporizador.Finish ()) {
-                Debug.Log ("Robot Colocaldo");
-                InitRobot ();
+    void Update()
+    {
+        if (temporizador.GetStage() == 0)
+        {
+            if (temporizador.Finish())
+            {
+                Debug.Log("Robot Colocaldo");
+                panelGlobal.SetActive(false);
+                texto.gameObject.SetActive(false);
+                ResetColorData();
+                InitRobot();
             }
         }
 
-        if (estado == GameState.running) {
-            if (RobotActual.transform.position != puntoFinal.position) {
-                RobotActual.transform.position = Vector3.Lerp (RobotActual.transform.position, puntoFinal.position, Time.deltaTime);
+        if (estado == GameState.running)
+        {
+            if (RobotActual.transform.position != puntoFinal.position)
+            {
+                RobotActual.transform.position = Vector3.Lerp(RobotActual.transform.position, puntoFinal.position, Time.deltaTime);
             }
-            for (int i = 0; i <= 11; i++) {
-                if (faja[i].childCount == 1 && faja[i].GetComponent<Faja> ().ruta == 10) {
-                    Instantiate (partes[Random.Range (0, partes.Length)], faja[i].position, faja[i].rotation, faja[i]);
+            for (int i = 0; i <= 11; i++)
+            {
+                if (faja[i].childCount == 1 && faja[i].GetComponent<Faja>().ruta == 10)
+                {
+                    Instantiate(partes[Random.Range(0, partes.Length)], faja[i].position, faja[i].rotation, faja[i]);
                 }
             }
 
             //timer --
-            if (temporizador.GetStage () == 1) {
-                temporizador.Activar (15);
-                if (temporizador.Finish ()) {
+            if (temporizador.GetStage() == 1)
+            {
+                temporizador.Activar(15);
+                if (temporizador.Finish())
+                {
                     estado = GameState.finish;
-                    SoundController.PlayOtherSoundEfect (12);
+                    SoundController.PlayOtherSoundEfect(12);
                 }
             }
 
             // if timer.runsout estado cambia a finish
         }
-        if (estado == GameState.finish) {
-            if (RobotActual.transform.position != puntoAparicion.position) {
-                RobotActual.transform.position = Vector3.Lerp (RobotActual.transform.position, puntoAparicion.position, Time.deltaTime);
-                if (puntoAparicion.position.y - RobotActual.transform.position.y < 0.5) {
-                    Destroy (RobotActual);
+        if (estado == GameState.finish)
+        {
+            if (RobotActual.transform.position != puntoAparicion.position)
+            {
+                RobotActual.transform.position = Vector3.Lerp(RobotActual.transform.position, puntoAparicion.position, Time.deltaTime);
+                if (puntoAparicion.position.y - RobotActual.transform.position.y < 0.5)
+                {
+                    Destroy(RobotActual);
                     //llamar temporizador
                     //Invoke("InitRobot", 5f);
-                    if (temporizador.GetStage () == 2) {
+                    if (temporizador.GetStage() == 2)
+                    {
                         estado = GameState.setup;
-                        temporizador.Activar (2f);
-                        temporizador.SetStage (0);
+                        temporizador.Activar(2f);
+                        temporizador.SetStage(0);
+                        BonusScore();
+                        panelGlobal.SetActive(true);
+                        texto.gameObject.SetActive(true);
                     }
                 }
             }
         }
 
     }
-    void SetPlayer () {
-        foreach (Player player in Jugadores) {
+    void SetPlayer()
+    {
+        foreach (Player player in Jugadores)
+        {
             player.robot = RobotActual.transform;
         }
     }
 
-    public void AparecerPieza () {
+    public void AparecerPieza()
+    {
         //   Instantiate (partes[Random.Range (0, partes.Length)], faja[i].position, faja[i].rotation, faja[i]);
     }
 
-    void InitRobot () {
-        RobotActual = Instantiate (Robots[Random.Range (0, Robots.Length)], puntoAparicion);
+    void InitRobot()
+    {
+        RobotActual = Instantiate(Robots[Random.Range(0, Robots.Length)], puntoAparicion);
         RobotActual.transform.parent = null;
-        SetPlayer ();
-        SoundController.PlayOtherSoundEfect (10);
+        SetPlayer();
+        SoundController.PlayOtherSoundEfect(10);
         estado = GameState.running;
         //llamado timer
     }
 
-    public void AddGlobalScore (int _value) {
+    public void AddGlobalScore(int _value)
+    {
         globalScore += _value;
+        texto.text = globalScore.ToString();
+
     }
 
-    public void AddPlayerData (string _ID, Nombre _nombre) {
-        switch (_ID) {
+    public void AddPlayerData(string _ID, Nombre _nombre)
+    {
+        switch (_ID)
+        {
             case "":
                 verificadorNombre[0] = _nombre;
                 break;
@@ -122,8 +158,10 @@ public class GameManager : MonoBehaviour {
                 break;
         }
     }
-    public void AddPlayerData (string _ID, string _piezaID) {
-        switch (_ID) {
+    public void AddPlayerData(string _ID, string _piezaID)
+    {
+        switch (_ID)
+        {
             case "":
                 verificadorPiezas[0] = _piezaID;
                 break;
@@ -139,8 +177,10 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    public void AddPlayerData (string _ID, int _color) {
-        switch (_ID) {
+    public void AddPlayerData(string _ID, int _color)
+    {
+        switch (_ID)
+        {
             case "":
                 verificadorColor[0] = _color;
                 break;
@@ -156,23 +196,51 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    public void BonusScore () {
-        multiplicador = 1;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 5; i++) {
-                if (i != j) {
+    public void ResetColorData()
+    {
+        for (int i = 0; i < verificadorNombre.Length; i++)
+        {
+            verificadorNombre[i] = Nombre.ninguno;
+        }
+    }
+
+    public void BonusScore()
+    {
+        multiplicador = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (i > j && verificadorNombre[i] != Nombre.ninguno)
+                {
+                    Debug.Log(i);
+                    Debug.Log(j);
+
                     if (verificadorColor[i] == verificadorColor[j])
+                    {
                         multiplicador++;
+                        Debug.Log(i);
+                        Debug.Log(j);
+                    }
                     if (verificadorNombre[i] == verificadorNombre[j])
+                    {
                         multiplicador++;
+                        Debug.Log(i);
+                        Debug.Log(j);
+                    }
                     if (verificadorPiezas[i] == verificadorPiezas[j])
+                    {
                         multiplicador++;
+                        Debug.Log(i);
+                        Debug.Log(j);
+                    }
                 }
             }
         }
-        multiplicador = multiplicador / 10f;
-        Debug.Log (globalScore);
+        multiplicador = 1 + (multiplicador / 10f);
+        Debug.Log(globalScore);
         globalScore = globalScore * multiplicador;
-        Debug.Log (globalScore);
+        Debug.Log(globalScore);
+        texto.text = globalScore.ToString();
     }
 }
