278d4d4541fc4fa6626717a81a9d60c13a92c5e3
Luis Pineda <luis.alonso97@outlook.com>
2020-02-02 12:47:00 -0500

    lloro

--- LimaGameJam2020/Assets/Scripts/Faja.cs
+++ LimaGameJam2020/Assets/Scripts/Faja.cs
@@ -2,58 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Faja : MonoBehaviour
-{
+public class Faja : MonoBehaviour {
     public int ruta;
     public Transform llegada;
     public float velocidad = 0.5f;
     public Transform direccion;
 
-
-
-
-    void Update()
-    {
+    void Update () {
         //transform.Translate(Vector3.right * Time.deltaTime * velocidad);
         if (!direccion) return;
 
-        transform.Translate(direccion.right * Time.deltaTime * velocidad);
+        transform.Translate (direccion.right * Time.deltaTime * velocidad);
 
-        if (transform.childCount >= 2)
-        {
-            transform.GetChild(1).transform.eulerAngles = Vector3.zero;
+        if (transform.childCount >= 2) {
+            transform.GetChild (1).transform.eulerAngles = Vector3.zero;
         }
         //ChildLookAt2D();
         if (!llegada) return;
-        LookAt2D();
+        LookAt2D ();
 
-        if (Vector3.Distance(transform.position, llegada.position) <= 0.5f)
-        {
-            CambioDireccion();
+        if (Vector3.Distance (transform.position, llegada.position) <= 0.5f) {
+            CambioDireccion ();
         }
         //GameManager.gm.DireccionFaja(ref transform);
     }
 
-    private void LookAt2D()
-    {
+    private void LookAt2D () {
         Vector3 dir = llegada.position - transform.position;
-        float angulo = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        direccion.rotation = Quaternion.AngleAxis(angulo, Vector3.forward);
+        float angulo = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg;
+        direccion.rotation = Quaternion.AngleAxis (angulo, Vector3.forward);
     }
 
-    private void ChildLookAt2D()
-    {
+    private void ChildLookAt2D () {
         if (transform.childCount <= 0) return;
-        Vector3 dir = transform.parent.position - transform.GetChild(0).transform.position;
-        float angulo = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.GetChild(0).transform.rotation = Quaternion.AngleAxis(angulo, Vector3.forward);
+        Vector3 dir = transform.parent.position - transform.GetChild (0).transform.position;
+        float angulo = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.GetChild (0).transform.rotation = Quaternion.AngleAxis (angulo, Vector3.forward);
     }
 
-    private void CambioDireccion()
-    {
+    private void CambioDireccion () {
         ruta++;
-        if (ruta >= GameManager.gm.ruta.Length)
+        if (ruta >= GameManager.gm.ruta.Length) {
             ruta = 0;
+            // GameManager.gm.AparecerPieza();
+        }
 
         llegada = GameManager.gm.ruta[ruta].transform;
     }
--- LimaGameJam2020/Assets/Scripts/GameManager.cs
+++ LimaGameJam2020/Assets/Scripts/GameManager.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     public GameObject[] Robots;
     public GameObject RobotActual;
     public Player[] Jugadores;
@@ -24,142 +23,144 @@ public class GameManager : MonoBehaviour
     public int[] verificadorColor;
     private float multiplicador;
 
-
-
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start () {
         if (!gm) gm = this;
         estado = GameState.setup;
 
-        Debug.Log("llamar");
-        temporizador.Activar(5f);
+        Debug.Log ("llamar");
+        temporizador.Activar (5f);
+        for (int i = 0; i <= 11; i++) {
+            if (faja[i].childCount == 1) {
+                Instantiate (partes[Random.Range (0, partes.Length)], faja[i].position, faja[i].rotation, faja[i]);
+            }
+        }
     }
     // Update is called once per frame
-    void Update()
-    {
-        if (temporizador.GetStage() == 0)
-        {
-            if (temporizador.Finish())
-            {
-                Debug.Log("Robot Colocaldo");
-                InitRobot();
+    void Update () {
+        if (temporizador.GetStage () == 0) {
+            if (temporizador.Finish ()) {
+                Debug.Log ("Robot Colocaldo");
+                InitRobot ();
             }
         }
 
-        if (estado == GameState.running)
-        {
-            if (RobotActual.transform.position != puntoFinal.position)
-            {
-                RobotActual.transform.position = Vector3.Lerp(RobotActual.transform.position, puntoFinal.position, Time.deltaTime);
+        if (estado == GameState.running) {
+            if (RobotActual.transform.position != puntoFinal.position) {
+                RobotActual.transform.position = Vector3.Lerp (RobotActual.transform.position, puntoFinal.position, Time.deltaTime);
             }
-            for (int i = 0; i <= 11; i++)
-            {
-                if (faja[i].childCount == 1)
-                {
-                    Instantiate(partes[Random.Range(0, partes.Length)], faja[i].position, faja[i].rotation, faja[i]);
+            for (int i = 0; i <= 11; i++) {
+                if (faja[i].childCount == 1 && faja[i].GetComponent<Faja> ().ruta == 10) {
+                    Instantiate (partes[Random.Range (0, partes.Length)], faja[i].position, faja[i].rotation, faja[i]);
                 }
             }
 
             //timer --
-            if (temporizador.GetStage() == 1)
-            {
-                temporizador.Activar(15);
-                if (temporizador.Finish())
-                {
+            if (temporizador.GetStage () == 1) {
+                temporizador.Activar (15);
+                if (temporizador.Finish ()) {
                     estado = GameState.finish;
-                    SoundController.PlayOtherSoundEfect(12);
+                    SoundController.PlayOtherSoundEfect (12);
                 }
             }
 
             // if timer.runsout estado cambia a finish
         }
-        if (estado == GameState.finish)
-        {
-            if (RobotActual.transform.position != puntoAparicion.position)
-            {
-                RobotActual.transform.position = Vector3.Lerp(RobotActual.transform.position, puntoAparicion.position, Time.deltaTime);
-                if (puntoAparicion.position.y - RobotActual.transform.position.y < 0.5)
-                {
+        if (estado == GameState.finish) {
+            if (RobotActual.transform.position != puntoAparicion.position) {
+                RobotActual.transform.position = Vector3.Lerp (RobotActual.transform.position, puntoAparicion.position, Time.deltaTime);
+                if (puntoAparicion.position.y - RobotActual.transform.position.y < 0.5) {
                     RobotActual = null;
                     //llamar temporizador
                     //Invoke("InitRobot", 5f);
-                    if (temporizador.GetStage() == 2)
-                    {
+                    if (temporizador.GetStage () == 2) {
                         estado = GameState.setup;
-                        temporizador.Activar(2f);
-                        temporizador.SetStage(0);
+                        temporizador.Activar (2f);
+                        temporizador.SetStage (0);
                     }
                 }
             }
         }
 
     }
-    void SetPlayer()
-    {
-        foreach (Player player in Jugadores)
-        {
+    void SetPlayer () {
+        foreach (Player player in Jugadores) {
             player.robot = RobotActual.transform;
         }
     }
 
-    void InitRobot()
-    {
-        RobotActual = Instantiate(Robots[Random.Range(0, Robots.Length)], puntoAparicion);
+    public void AparecerPieza () {
+        //   Instantiate (partes[Random.Range (0, partes.Length)], faja[i].position, faja[i].rotation, faja[i]);
+    }
+
+    void InitRobot () {
+        RobotActual = Instantiate (Robots[Random.Range (0, Robots.Length)], puntoAparicion);
         RobotActual.transform.parent = null;
-        SetPlayer();
-        SoundController.PlayOtherSoundEfect(10);
+        SetPlayer ();
+        SoundController.PlayOtherSoundEfect (10);
         estado = GameState.running;
         //llamado timer
     }
 
-    public void AddGlobalScore(int _value)
-    {
+    public void AddGlobalScore (int _value) {
         globalScore += _value;
     }
 
-    public void AddPlayerData(string _ID, Nombre _nombre)
-    {
-        switch (_ID)
-        {
-            case "": verificadorNombre[0] = _nombre; break;
-            case "2P": verificadorNombre[1] = _nombre; break;
-            case "3P": verificadorNombre[2] = _nombre; break;
-            case "4P": verificadorNombre[3] = _nombre; break;
+    public void AddPlayerData (string _ID, Nombre _nombre) {
+        switch (_ID) {
+            case "":
+                verificadorNombre[0] = _nombre;
+                break;
+            case "2P":
+                verificadorNombre[1] = _nombre;
+                break;
+            case "3P":
+                verificadorNombre[2] = _nombre;
+                break;
+            case "4P":
+                verificadorNombre[3] = _nombre;
+                break;
         }
     }
-    public void AddPlayerData(string _ID, string _piezaID)
-    {
-        switch (_ID)
-        {
-            case "": verificadorPiezas[0] = _piezaID; break;
-            case "2P": verificadorPiezas[1] = _piezaID; break;
-            case "3P": verificadorPiezas[2] = _piezaID; break;
-            case "4P": verificadorPiezas[3] = _piezaID; break;
+    public void AddPlayerData (string _ID, string _piezaID) {
+        switch (_ID) {
+            case "":
+                verificadorPiezas[0] = _piezaID;
+                break;
+            case "2P":
+                verificadorPiezas[1] = _piezaID;
+                break;
+            case "3P":
+                verificadorPiezas[2] = _piezaID;
+                break;
+            case "4P":
+                verificadorPiezas[3] = _piezaID;
+                break;
         }
     }
 
-    public void AddPlayerData(string _ID, int _color)
-    {
-        switch (_ID)
-        {
-            case "": verificadorColor[0] = _color; break;
-            case "2P": verificadorColor[1] = _color; break;
-            case "3P": verificadorColor[2] = _color; break;
-            case "4P": verificadorColor[3] = _color; break;
+    public void AddPlayerData (string _ID, int _color) {
+        switch (_ID) {
+            case "":
+                verificadorColor[0] = _color;
+                break;
+            case "2P":
+                verificadorColor[1] = _color;
+                break;
+            case "3P":
+                verificadorColor[2] = _color;
+                break;
+            case "4P":
+                verificadorColor[3] = _color;
+                break;
         }
     }
 
-
-    public void BonusScore()
-    {
+    public void BonusScore () {
         multiplicador = 1;
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 5; i++)
-            {
-                if (i != j)
-                {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; i++) {
+                if (i != j) {
                     if (verificadorColor[i] == verificadorColor[j])
                         multiplicador++;
                     if (verificadorNombre[i] == verificadorNombre[j])
@@ -170,8 +171,8 @@ public class GameManager : MonoBehaviour
             }
         }
         multiplicador = multiplicador / 10f;
-        Debug.Log(globalScore);
+        Debug.Log (globalScore);
         globalScore = globalScore * multiplicador;
-        Debug.Log(globalScore);
+        Debug.Log (globalScore);
     }
 }
