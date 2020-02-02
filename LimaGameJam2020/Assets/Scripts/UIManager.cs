14fcc004537be46935be0f0e6e803a57615d40c6
Luis Pineda <luis.alonso97@outlook.com>
2020-02-02 10:40:11 -0500

    holi

---
+++ LimaGameJam2020/Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    public Transform feedback_giro;
+    public Transform feedback_mash;
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    
+}
